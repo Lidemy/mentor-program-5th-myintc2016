cc94ea8683bff8762675921174857bed71d69e5f
myintc2016 <mia5201@gmail.com>
2021-05-04 02:41:53 +0800

    finish week3

---
+++ homeworks/week3/hw1.js
@@ -0,0 +1,25 @@
+/* eslint-disable eol-last */
+const readline = require('readline')
+
+const lines = []
+const rl = readline.createInterface({
+  input: process.stdin
+})
+
+rl.on('line', (line) => {
+  lines.push(line)
+})
+
+rl.on('close', () => {
+  solve(lines)
+})
+
+function solve(lines) {
+  const n = lines
+  let ans = ''
+  for (let i = 1; i <= n; i++) {
+    ans += '*'
+    console.log(ans)
+  }
+  return ans
+}
---
+++ homeworks/week3/hw4.js
@@ -0,0 +1,23 @@
+/* eslint-disable eol-last */
+const readline = require('readline')
+
+const lines = []
+const rl = readline.createInterface({
+  input: process.stdin
+})
+
+rl.on('line', (line) => {
+  lines.push(line)
+})
+
+rl.on('close', () => {
+  solve(lines)
+})
+
+function isPalindrome(str) {
+  return str === str.split('').reverse().join('')
+}
+
+function solve(lines) {
+  console.log(isPalindrome(lines[0]) ? 'True' : 'False')
+}
